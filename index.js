f4a8c6d15f182f676ddf36b54bfbc49103a1305d
Reilly Shofner <reilly.shofner@gmail.com>
2024-03-19 14:41:39 -0700

    complete phase-1-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function nameCustomer() {
+    return customerName;
+}
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+   return customerName;
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+console.log(bestCustomer);
+
+const leastFavoriteCustomer = 'someone' 
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'someone else';
+    return leastFavoriteCustomer;
+}
+
+console.log(leastFavoriteCustomer);
